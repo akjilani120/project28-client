59b59481ef7861a467f5349870cad6e25c93c1f7
unknown <abdulkaderjilani120@gmail.com>
2022-08-10 18:23:46 +0600

    added start

---
+++ src/Page/ArBoard/AtBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AtBoard = () => {
+    return (
+        <div>
+            <h1>I am At Board</h1>
+        </div>
+    );
+};
+
+export default AtBoard;
---
+++ src/Page/Login/Registation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Registation = () => {
+    return (
+        <div>
+            <h1>I am Registation</h1>
+        </div>
+    );
+};
+
+export default Registation;
